3ba3f8c7c5c58be2446dbe0eb945388a2b540ced
Lee Seung Hee <kye1281@naver.com>
2025-01-23 16:17:35 +0900

    cached delete files 2

--- target/classes/static/js/toast.js
+++
@@ -1,9 +0,0 @@
-/* ********************************************************
-* 함수명	: fn_showToastMessage(string)
-* 설명	: 인자로 넘겨받은 문자열을 토스트 메시지로 보여줌
-******************************************************** */
-function fn_showToastMessage(string) {
-    let toast = $('#base-toast');
-    toast.find('.toast-body').html(string); // 토스트 메시지 세팅
-    toast.toast('show'); // 토스트 표시
-}
